a4892b9347e2fd8f8385bf8aa2b701edebfbdd46
Aryel Tupinamba <aryel.tupinamba@lqdi.net>
2018-01-27 23:46:28 -0200

    added some sfx; added basic hp hud and health/damage system; recalibrated movement and jumping

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Entities;
 using Structs;
 using UnityEngine;
 
@@ -10,7 +11,10 @@ public class Bullet : MonoBehaviour {
 
 	public TeamID teamID;
 	public int lifetime = 0;
+	public int damage = 10;
 	public bool bounce = false;
+	
+	public AudioClip[] sfxBulletHit;
 
 	public void Fire(Vector3 start, float aimX, float aimY, TeamID team) {
 		teamID = team;
@@ -28,11 +32,30 @@ public class Bullet : MonoBehaviour {
 			Destroy(gameObject);
 		}
 	}
+	
+	public void PlaySFX(AudioClip clip, float volume = 1.0f) {
+		AudioSource.PlayClipAtPoint(clip, transform.position, volume);
+	}
 
 	public void OnCollisionEnter(Collision collision) {
+
+		if (collision.gameObject.CompareTag("Player")) {
+			collision.gameObject.GetComponent<Player>().ApplyDamage(damage);
+			Destroy(gameObject);
+			return;
+		}
+
+		if (collision.gameObject.CompareTag("Enemy")) {
+			// TODO: apply damage
+			Destroy(gameObject);
+		};
 		
-		if (collision.gameObject.CompareTag("Player") && teamID == TeamID.Player) return;
-		if (collision.gameObject.CompareTag("Enemy") && teamID == TeamID.Enemy) return;
+		// TODO: meat hit sfx
+		// TODO: particle emit
+		
+		AudioClip hitClip = sfxBulletHit[Random.Range(0, sfxBulletHit.Length - 1)];
+		
+		PlaySFX(hitClip);
 		
 		if(!bounce) Destroy(gameObject);
 	}
